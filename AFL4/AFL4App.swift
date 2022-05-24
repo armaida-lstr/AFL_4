90e184044724e7c72bf707c41e3022dfea157c1c
armaidalstr <armaidalstr@gmail.com>
2022-05-24 08:53:06 +0700

    Initial Commit

---
+++ AFL4/AFL4App.swift
@@ -0,0 +1,17 @@
+//
+//  AFL4App.swift
+//  AFL4
+//
+//  Created by MacBook Pro on 24/05/22.
+//
+
+import SwiftUI
+
+@main
+struct AFL4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
